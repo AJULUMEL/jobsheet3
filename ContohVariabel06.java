6b55981170e5176c19109becde5fae2e9b5751ff
unknown <AJULUMEL>
2023-10-05 01:02:03 -0700

    first commit

---
+++ ContohVariabel06.java
@@ -0,0 +1,15 @@
+public class ContohVariabel06 {
+    public static void main(String[] args) {
+
+        String Hobi = "Bermain petak umpet";
+        boolean Pandai = true;
+        char jenisKelamin = 'L';
+        byte umur = 20;
+        double ipkSaya = 3.24, tinggi = 1.78;
+        System.out.println("Hobi saya adalah: " + Hobi);
+        System.out.println("Apakah pandai? " + Pandai);
+        System.out.println("Jenis kelamin: " + jenisKelamin);
+        System.out.println("Umurku saat ini: " + umur);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", ipkSaya, tinggi));
+    }
+}
